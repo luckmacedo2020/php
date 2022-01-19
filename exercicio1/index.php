9a56bae3282cb37127a1a6bebe8bd72be8fe61d7
luckmacedo2020 <luckcosta99@gmail.com>
2022-01-19 09:14:26 -0300

    exercicio 1

---
+++ exercicio1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fundamentos bascios php</title>
+</head>
+<body>
+    <?php
+    /////Esse é meu primeiro arquivo php
+    echo "meu nome é luciano<br>";
+
+    print "minha idade é 25";
+
+
+    ?>
+</body>
+</html>
