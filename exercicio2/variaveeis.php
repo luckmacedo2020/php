d7f7a3eeb3cee62949e83ae0b4e71a2290c945ba
luckmacedo2020 <luckcosta99@gmail.com>
2022-01-19 09:23:42 -0300

    exercicio 2 variaveis

---
+++ exercicio2/variaveeis.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fundamentos bascios php</title>
+</head>
+<body>
+    <?php
+    $nome = "Luciano Da Costa Macedo";
+    $idade = 25;
+    $altura = 1.60;
+
+    echo "Meu nome é $nome minha idade é $idade minha altura é $altura" ;
+
+
+    ?>
+</body>
+</html>
